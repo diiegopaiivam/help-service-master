fdee1c05081547ac2d2a30fd3e98de0c91de6eb5
Diego Paiva <diiegopaiivam@gmail.com>
2019-07-23 01:42:34 -0300

    Configurando bootstrap e style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+
 //Components
 
 import { PaginainicialComponent } from './paginainicial/paginainicial.component';
 import { CadastrarComponent } from './cadastro/cadastrar/cadastrar.component';
 import { CadastroclienteComponent } from './cadastro/cadastrocliente/cadastrocliente.component';
 import { CadastroprestadorComponent } from './cadastro/cadastroprestador/cadastroprestador.component';
+
+
 
 
 
